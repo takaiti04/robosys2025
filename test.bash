a10c41581994e841bc2e3e29d38b558c003d8e89
Takagi Issei <yayayatititiyau@gmail.com>
2025-10-30 13:41:14 +0900

    更新

--- test.bash
+++ test.bash
@@ -2,15 +2,16 @@
 # SPDX-FileCopyrightText: 2025 Takagi Issei
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-	echo ${1}行目が違うよ
-	res=1
-}
-
-res=0
-
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = ""]  || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "LINENO"
+[ "${out}" = "" ] || ng "LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
