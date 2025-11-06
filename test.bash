af7a64dee97370a985523e9ef1eb252844cc1a80
Takagi Issei <yayayatititiyau@gmail.com>
2025-11-06 12:09:03 +0900

    更新

--- test.bash
+++ test.bash
@@ -1,21 +1,21 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2025 Takagi Issei
-# SPDX-License-Identifier: BSD-3-Clause
+#SPDX-FileCopyrightText: 2025 Takagi Issei
+#SPDX-License-Identifier: BSD-3-Clause
 ng () {
-       echo ${1}行目が違うよ
-       res=1
+        echo ${1}行目が違うよ
+        res=1
 }
+
 res=0
+###NORMAL INPUT###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
-
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = ""]  || ng "$LINENO"
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "LINENO"
-[ "${out}" = "" ] || ng "LINENO"
-
+[ "${out}" = 15 ]  ng "$LINENO"
+###STRANGE INPUT###
+out=$(echo あ | ./plus)           #計算できない値を入力してみる
+[ "$?" = 1 ]       ng "$LINENO" #終了ステータスが1なのを確認
+[ "${out}" = "" ]  ng "$LINENO"
+out=$(echo | ./plus)              #なにも入力しない
+[ "$?" = 1 ]       ng "$LINENO" #これも異常終了する
+[ "${out}" = "" ] || ng "$LINENO"
 [ "$res" = 0 ] && echo OK
 exit $res
